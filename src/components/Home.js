781bb4e66f86289a69ab1ce2d9c8459f694cefc9
Nephthali-Tutondele <nephtalitutondele@gmail.com>
2024-05-21 17:10:38 +0100

    dddfddffdddddddfgfhdddddf

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
 
-   const baseUrl = process.env.REACT_APP_DATAURL;
+   const baseUrl = process.env.REACT_APP_DATA_URL;
   return (
 
     <div className='p-3'>
